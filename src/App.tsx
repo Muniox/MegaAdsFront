a819fe6f284176da9ec9a37b77f4442bff2cc0d6
Muniox <muniox20@gmail.com>
2023-02-02 15:56:10 +0100

    Basic structure of main view.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,23 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import React from 'react';
 import './App.pcss';
 
-
 export const App = () => {
-  const [count, setCount] = useState(0);
-  const d = import.meta.env.VITE_SOME_KEY;
-  console.log(d);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <header>
+        <h1>
+          <span>Mega</span> Ogłoszenia
+        </h1>
+        <button>Dodaj Ogłoszenie</button>
+        <div className="search">
+          <input type="text" />
+          <button>Szukaj</button>
+        </div>
+      </header>
+      <div className="map">
+        ...
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   );
 };
 
