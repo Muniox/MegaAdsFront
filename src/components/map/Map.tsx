4333ee18dc2af3afddd57e69efc9cf245b427535
Muniox <muniox20@gmail.com>
2023-02-11 12:49:28 +0100

    add some react elements

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import './Map.pcss';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { SearchContext } from '../../context/searchContext';
 
 export const Map = () => {
+    const {search} = useContext(SearchContext);
+
+    useEffect(() => {
+        console.log('Make request to search for', search);
+    },[search]);
+
     return (
         <div className="map">
+            <h1>Search for: {search}</h1>
             <MapContainer center={[50.2655229,18.9938733]} zoom={20}>
                 <TileLayer 
                     url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
